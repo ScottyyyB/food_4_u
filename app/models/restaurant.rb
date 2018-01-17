b985c9fd8f183b6710ed6260529d8718675032a7
aljoh <reachingalfred@gmail.com>
2018-01-17 21:37:25 +0100

    unit testing for geocoder in progress

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,12 @@ class Restaurant < ApplicationRecord
   has_many :menus
   validates :name, presence: true
   validates :description, presence: true
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    Geocoder.address([self.street_address,
+                      self.post_code,
+                      self.city].join(','))
+  end
 end
