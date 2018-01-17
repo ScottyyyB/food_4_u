3c2278ada4feda7f13d529f9359da39d18481315
ScottB <17sb@cis.dk>
2018-01-17 11:57:45 +0100

    Style product area and align order button to the right

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116144940) do
+ActiveRecord::Schema.define(version: 20180116160024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180116144940) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "assortment"
   end
 
   add_foreign_key "menus", "restaurants"
