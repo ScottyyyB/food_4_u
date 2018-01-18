fd0d5104f660f9ee2dfa217a5d50baf9797623c0
ScottB <17sb@cis.dk>
2018-01-18 14:12:16 +0100

    Finish refactor, remove restaurants search action and moved to index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,5 @@
 class RestaurantsController < ApplicationController
-
   def index
-    binding.pry
-
     if params[:search]
       @assortment = params[:search]
       @results = Restaurant.all.select { |restaurant| restaurant.assortment.include? @assortment.capitalize }
@@ -15,12 +12,4 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @menus = @restaurant.menus.all
   end
-
-  # def search
-  #   @assortment = params[:assortment]
-  #   @results = Restaurant.all.select { |restaurant| restaurant.assortment.include? @assortment.capitalize }
-  #   flash[:danger] = 'No restaurants found.' if @results.none?
-  #   render :index
-  # end
-
 end
