536f7144738cf13cb2a849f187f56053a2c20bd0
aljoh <reachingalfred@gmail.com>
2018-01-20 16:35:56 +0100

    stubbing unit test for geocoder in progress

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,20 @@
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
 require 'coveralls'
 Coveralls.wear_merged!('rails')
 RSpec.configure do |config|
+  #fixtures_path = "#{::Rails.root}/features/support/fixtures"
+
+  stockholm_3 = stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=Hantverkargatan%2028,11220,Stockholm&key=AIzaSyCHgPt-pc3Mt8xSRhj94gV6Ge1X9zJpvzg&language=en&sensor=false")
+  config.before(:each) do
+    stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=Hantverkargatan%2028,11220,Stockholm&key=AIzaSyCHgPt-pc3Mt8xSRhj94gV6Ge1X9zJpvzg&language=en&sensor=false").
+        with(  headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Ruby'
+        }).
+        to_return(status: 200, body: stockholm_3, headers: {})
+  end
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
