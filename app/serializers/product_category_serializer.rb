91589a4ddb03b6be5373bac25d735339beff0365
aljoh <reachingalfred@gmail.com>
2018-01-24 22:01:41 +0100

    creates nested products with serializer

--- app/serializers/product_category_serializer.rb
+++ app/serializers/product_category_serializer.rb
@@ -3,4 +3,9 @@ class ProductCategorySerializer < ActiveModel::Serializer
   attribute :products
   belongs_to :menu
 
+  def products
+    object.products.map do |product|
+      ProductSerializer.new(product).as_json
+    end
+  end
 end
